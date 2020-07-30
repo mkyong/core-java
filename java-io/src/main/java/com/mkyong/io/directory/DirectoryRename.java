d1fe336b99b8da89262a67f5a7fa5ddfdf693d59
mkyong <mkyong2002@gmail.com>
2020-07-30 17:48:08 +0800

    rename file

---
+++ java-io/src/main/java/com/mkyong/io/directory/DirectoryRename.java
@@ -0,0 +1,29 @@
+package com.mkyong.io.directory;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class DirectoryRename {
+
+    public static void main(String[] args) {
+
+        String file1 = "/home/mkyong/hello.txt";
+        String file2 = "/home/mkyong/newfolder/hello2.txt";
+
+        Path source = Paths.get(file1);
+        Path target = Paths.get(file2);
+
+        try {
+
+            // rename a file in the same directory
+            Files.move(source, source.resolveSibling("newName.txt"));
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
