ba52360c0578076964835b02e138dc64da5d013b
mkyong <mkyong2002@gmail.com>
2020-09-03 22:50:18 +0800

    read file from resources

--- java-io/src/main/java/com/mkyong/io/utils/FileResourcesUtils.java
+++ java-io/src/main/java/com/mkyong/io/utils/FileResourcesUtils.java
@@ -1,19 +1,25 @@
 package com.mkyong.io.utils;
 
 import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
+import java.nio.file.FileSystem;
+import java.nio.file.*;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FileResourcesUtils {
 
     public static void main(String[] args) throws IOException {
 
+        FileResourcesUtils app = new FileResourcesUtils();
+
+        // Sample 1 - read a file
         //String fileName = "database.properties";
         String fileName = "json/file1.json";
-
-        FileResourcesUtils app = new FileResourcesUtils();
 
         System.out.println("getResourceAsStream : " + fileName);
         InputStream is = app.getFileFromResourceAsStream(fileName);
@@ -23,32 +29,60 @@ public class FileResourcesUtils {
         File file = app.getFileFromResource(fileName);
         printFile(file);
 
-        // get all files from a resources folder (JAR version)
+        // Sample 2 - Read all files from a resources folder (Not working in JAR)
         /*try {
 
-            List<Path> result = app.getPathsFromResourceJAR();
+            // src/main/resources/json
+            List<File> result = app.getAllFilesFromResource("json");
+            for (File file : result) {
+                System.out.println("file : " + file);
+                printFile(file);
+            }
+
+        } catch (URISyntaxException | IOException e) {
+            e.printStackTrace();
+        }*/
+
+        // Sample 3 - read all files from a resources folder (JAR version)
+        /*try {
+
+            // get filename from src/main/resources/json
+            List<Path> result = app.getPathsFromResourceJAR("json");
             for (Path path : result) {
                 System.out.println("Path : " + path);
-                InputStream is = app.getFileFromResourceAsStream(path.toString());
+
+                String filePathInJAR = path.toString();
+                // Windows will returns /json/file1.json, cut the first /
+                // the correct path should be json/file1.json
+                if (filePathInJAR.startsWith("/")) {
+                    filePathInJAR = filePathInJAR.substring(1);
+                }
+
+                System.out.println("filePathInJAR : " + filePathInJAR);
+
+                // read a file from resource folder
+                InputStream is = app.getFileFromResourceAsStream(filePathInJAR);
                 printInputStream(is);
             }
 
-        } catch (URISyntaxException e) {
+        } catch (URISyntaxException | IOException e) {
             e.printStackTrace();
         }*/
 
     }
 
-
     // get a file from resources folder
     // works in everywhere, IDEA, unit test and JAR file.
     private InputStream getFileFromResourceAsStream(String fileName) {
 
+        // for static access
+        //ClassLoader classLoader1 = FileResourcesUtils.class.getClassLoader();
+
         // The class loader that loaded the class
         ClassLoader classLoader = getClass().getClassLoader();
-
         InputStream inputStream = classLoader.getResourceAsStream(fileName);
 
+        // the stream holding the file content
         if (inputStream == null) {
             throw new IllegalArgumentException("file not found! " + fileName);
         } else {
@@ -57,13 +91,16 @@ public class FileResourcesUtils {
 
     }
 
-    // The a resource URL is not working in JAR file
-    // Exception in thread "main" java.nio.file.NoSuchFileException:
-    // file:/home/mkyong/projects/core-java/java-io/target/java-io.jar!/json/file1.json
+    /*
+        The resource URL is not working in JAR
+        If we try to access a file that is inside a JAR,
+        It throws NoSuchFileException (linux), InvalidPathException (Windows)
+
+        Resource URL Sample: file:java-io.jar!/json/file1.json
+     */
     private File getFileFromResource(String fileName) {
 
         ClassLoader classLoader = getClass().getClassLoader();
-
         URL resource = classLoader.getResource(fileName);
         if (resource == null) {
             throw new IllegalArgumentException("file not found! " + fileName);
@@ -73,21 +110,23 @@ public class FileResourcesUtils {
 
     }
 
-    // Get all path from the JAR file
-    /*private List<Path> getPathsFromResourceJAR() throws URISyntaxException, IOException {
+    // Get all paths from a folder that inside the JAR file
+    private List<Path> getPathsFromResourceJAR(String folder) throws URISyntaxException, IOException {
 
         List<Path> result;
 
-        // get path of the running JAR
-        String jarPath = getClass().getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
-        System.out.println("path :" + jarPath);
+        // get path of the current running JAR
+        String jarPath = getClass().getProtectionDomain()
+                .getCodeSource()
+                .getLocation()
+                .toURI()
+                .getPath();
+        System.out.println("JAR Path :" + jarPath);
 
+        // file walks JAR
         URI uri = URI.create("jar:file:" + jarPath);
-
         try (FileSystem fs = FileSystems.newFileSystem(uri, Collections.emptyMap())) {
-
-            // get paths from src/main/resources/json
-            result = Files.walk(fs.getPath("json"))
+            result = Files.walk(fs.getPath(folder))
                     .filter(Files::isRegularFile)
                     .collect(Collectors.toList());
         }
@@ -96,27 +135,26 @@ public class FileResourcesUtils {
 
     }
 
-    // Works in IDE and unit test, not working in JAR
-    private List<File> getAllFilesFromResource() throws URISyntaxException, IOException {
+    // Normal file walk is not working in JAR
+    private List<File> getAllFilesFromResource(String folder) throws URISyntaxException, IOException {
 
         ClassLoader classLoader = getClass().getClassLoader();
 
-        // src/main/resources/json
-        URL resource = classLoader.getResource("json");
+        URL resource = classLoader.getResource(folder);
 
+        // dun walk the root path, we will walk all the classes
         List<File> collect = Files.walk(Paths.get(resource.toURI()))
                 .filter(Files::isRegularFile)
                 .map(x -> x.toFile())
                 .collect(Collectors.toList());
 
         return collect;
-    }*/
+    }
 
     // print input stream
-    private static void printInputStream(InputStream inputStream) throws IOException {
+    private static void printInputStream(InputStream is) {
 
-        try (InputStreamReader streamReader =
-                     new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+        try (InputStreamReader streamReader = new InputStreamReader(is, StandardCharsets.UTF_8);
              BufferedReader reader = new BufferedReader(streamReader)) {
 
             String line;
@@ -124,12 +162,15 @@ public class FileResourcesUtils {
                 System.out.println(line);
             }
 
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
     }
 
     // print a file
     private static void printFile(File file) {
+
         List<String> lines;
         try {
             lines = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
