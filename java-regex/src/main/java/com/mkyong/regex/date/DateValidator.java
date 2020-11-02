c839794e028538e13ba5eb8a987f12e8463438df
mkyong <mkyong2002@gmail.com>
2020-11-02 23:46:37 +0800

    regex date

---
+++ java-regex/src/main/java/com/mkyong/regex/date/DateValidator.java
@@ -0,0 +1,60 @@
+package com.mkyong.regex.date;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DateValidator {
+
+    // ?: match but don't capture it
+    // yyyy-mm-dd
+    private static final String DATE_PATTERN =
+            "^((?:19|20)[0-9][0-9])-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])$";
+
+    private static final Pattern pattern = Pattern.compile(DATE_PATTERN);
+
+    public static boolean isValid(final String date) {
+
+        boolean result = false;
+
+        Matcher matcher = pattern.matcher(date);
+
+        if (matcher.matches()) {
+
+            // it is a valid date format yyyy-mm-dd
+            // assign true first, later we will check the leap year and odd or even months
+            result = true;
+
+            // (?:19|20), match but don't capture it, otherwise it will messy the group order
+            // for example, 2020-2-30, it will create 4 groups.
+            // group(1) = 2020, group(2) matches (19|20) = 20, group(3) = 2, group(4) = 30
+            // So, we put (?:19|20), don't capture this group.
+            int year = Integer.parseInt(matcher.group(1));
+            // why string? month matches 02 or 2
+            String month = matcher.group(2);
+            String day = matcher.group(3);
+
+            // 30 or 31 days checking
+            // only 1,3,5,7,8,10,12 has 31 days
+            if ((month.equals("4") || month.equals("6") || month.equals("9") ||
+                    month.equals("04") || month.equals("06") || month.equals("09") ||
+                    month.equals("11")) && day.equals("31")) {
+                result = false;
+            } else if (month.equals("2") || month.equals("02")) {
+                if (day.equals("30") || day.equals("31")) {
+                    result = false;
+                } else if (day.equals("29")) {  // leap year, feb 29 days.
+                    if (!isLeapYear(year)) {
+                        result = false;
+                    }
+                }
+            }
+
+        }
+
+        return result;
+    }
+
+    private static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
+    }
+}
---
+++ java-regex/src/test/java/com/mkyong/regex/date/DateValidatorTest.java
@@ -0,0 +1,64 @@
+package com.mkyong.regex.date;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DateValidatorTest {
+
+    @ParameterizedTest(name = "#{index} - Run test with date = {0}")
+    @MethodSource("validDateProvider")
+    void test_date_valid(String date) {
+        assertTrue(DateValidator.isValid(date));
+    }
+
+    @ParameterizedTest(name = "#{index} - Run test with date = {0}")
+    @MethodSource("invalidDateProvider")
+    void test_date_invalid(String date) {
+        assertFalse(DateValidator.isValid(date));
+    }
+
+    static Stream<String> validDateProvider() {
+        return Stream.of(
+                "1998-09-30",
+                "1998-9-30",
+                "2020-09-1",
+                "2020-09-01",
+                "2020-9-1",
+                "2020-9-01",
+                "2020-2-29",             // leap year
+                "2020-2-28",             // leap year
+                "2019-2-28",             // common year
+                "2000-02-29",            // 2000 is a leap year, % 400 == 0
+                "1900-02-28",            // 1900 is a common year
+                "2020-07-31",
+                "2020-08-31",
+                "2020-06-30",
+                "1900-01-01",
+                "2099-12-31");
+    }
+
+    static Stream<String> invalidDateProvider() {
+        return Stream.of(
+                "1998-09-31",               // invalid day, sep max 30
+                "1998-11-31",               // invalid day, nov max 30
+                "2008-02-2x",               // invalid day 2x
+                "2008-0x-28",               // invalid month 0x
+                "20xx-02-28",               // invalid year 20xx
+                "20-11-02",                 // invalid year 20, must be yyyy
+                "2020/11/02",               // invalid date format, yyyy-mm-dd
+                "2020-11-32",               // invalid day, 32
+                "2020-13-30",               // invalid month 13
+                "2020-A-20",                // invalid month A
+                "2020-2-30",                // leap year, feb max 29
+                "2019-2-29",                // common year, feb max 28
+                "1900-02-29",               // 1900 is a common year, feb max 28
+                " ",                        // empty
+                "");                        // empty
+    }
+
+}
