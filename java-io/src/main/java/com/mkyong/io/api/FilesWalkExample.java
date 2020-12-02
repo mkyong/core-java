d15a266ad858e55c30f507ade90caedd6fe3cfac
mkyong <mkyong2002@gmail.com>
2020-12-02 23:09:18 +0800

    files.walk

--- java-io/src/main/java/com/mkyong/io/api/FilesWalkExample.java
+++ java-io/src/main/java/com/mkyong/io/api/FilesWalkExample.java
@@ -7,8 +7,6 @@ import java.nio.file.Paths;
 import java.nio.file.attribute.FileTime;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
-import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 // Files.walk example
@@ -17,26 +15,61 @@ public class FilesWalkExample {
     public static void main(String[] args) throws IOException {
 
         Path path = Paths.get("C:\\test\\");
-        setModifiedDate(path, Instant.now().minus(1, ChronoUnit.DAYS));
+        //List<Path> paths = listFiles(path);
+        //List<Path> paths = listDirectories(path);
+        //List<Path> paths = findByFileExtension(path, ".txt");
+        //List<Path> paths = findByFileName(path, "readme.txt");
+
+        //long fileSizeInBytes = 1024 * 1024 * 10; // 10MB
+        //List<Path> paths = findByFileSize(path, fileSizeInBytes);
+        //paths.forEach(x -> System.out.println(x));
+
+        Instant yesterday = Instant.now().minus(1, ChronoUnit.DAYS);
+        setAllFilesModifiedDate(path, yesterday);
 
     }
 
-    // set last modified time of all files from this path
-    public static void setModifiedDate(Path path, Instant instant) throws IOException {
+    // list all files from this path
+    /*public static List<Path> listFiles(Path path) throws IOException {
 
+        List<Path> result;
+        //try (Stream<Path> walk = Files.walk(path, 1)) {
         try (Stream<Path> walk = Files.walk(path)) {
-            walk
-                    .filter(Files::isReadable)      // read permission
-                    .filter(Files::isRegularFile)   // is a file
-                    .forEach(p -> {
-                        // set last modified time
-                        try {
-                            Files.setLastModifiedTime(p, FileTime.from(instant));
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    });
+            result = walk.filter(Files::isRegularFile)
+                    .collect(Collectors.toList());
         }
+        return result;
+
+    }*/
+
+    // list all directories from this path
+    /*public static List<Path> listDirectories(Path path) throws IOException {
+
+        List<Path> result;
+        try (Stream<Path> walk = Files.walk(path)) {
+            result = walk.filter(Files::isDirectory)
+                    .collect(Collectors.toList());
+        }
+        return result;
+
+    }
+
+    public static List<Path> findByFileExtension(Path path, String fileExtension)
+            throws IOException {
+
+        if (!Files.isDirectory(path)) {
+            throw new IllegalArgumentException("Path must be a directory!");
+        }
+
+        List<Path> result;
+        try (Stream<Path> walk = Files.walk(path)) {
+            result = walk
+                    .filter(Files::isRegularFile)   // is a file
+                    .filter(p -> p.getFileName().toString().endsWith(fileExtension))
+                    .collect(Collectors.toList());
+        }
+        return result;
+
     }
 
     public static List<Path> findByFileName(Path path, String fileName) throws IOException {
@@ -56,10 +89,10 @@ public class FilesWalkExample {
         }
         return result;
 
-    }
+    }*/
 
     // fileSize in bytes
-    public static List<Path> findByFileSize(Path path, long fileSize) throws IOException {
+    /*public static List<Path> findByFileSize(Path path, long fileSize) throws IOException {
 
         if (!Files.isDirectory(path)) {
             throw new IllegalArgumentException("Path must be a directory!");
@@ -88,6 +121,24 @@ public class FilesWalkExample {
             System.err.println("Unable to get the file size of this file: " + path);
         }
         return result;
+    }*/
+
+    // set all files' last modified time to this instant
+    public static void setAllFilesModifiedDate(Path path, Instant instant) throws IOException {
+
+        try (Stream<Path> walk = Files.walk(path)) {
+            walk
+                    .filter(Files::isReadable)      // read permission
+                    .filter(Files::isRegularFile)   // file only
+                    .forEach(p -> {
+                        try {
+                            // set last modified time
+                            Files.setLastModifiedTime(p, FileTime.from(instant));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+        }
     }
 
 }
