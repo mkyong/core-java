a9cebec1d239e88835308af864b1ebe0560411e1
mkyong <mkyong2002@gmail.com>
2021-05-11 20:23:55 +0800

    x

--- java-xml/src/main/java/com/mkyong/xml/jdom/ModifyXmlJDom.java
+++ java-xml/src/main/java/com/mkyong/xml/jdom/ModifyXmlJDom.java
@@ -1,15 +1,18 @@
 package com.mkyong.xml.jdom;
 
-import org.jdom2.*;
+import org.jdom2.CDATA;
+import org.jdom2.Content;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.ListIterator;
 
 public class ModifyXmlJDom {
 
@@ -25,18 +28,23 @@ public class ModifyXmlJDom {
         List<Element> listChildrenNode = rootNode.getChildren("staff");
 
         // staff = 2
-        System.out.println("No of child nodes: " + listChildrenNode.size());
+        // System.out.println("No of child nodes: " + listChildrenNode.size());
 
         // loop the elements
         for (Element staff : listChildrenNode) {
 
             String id = staff.getAttribute("id").getValue();
 
+            // if name = mkyong, remove the staff node
+            /*if ("mkyong".equals(staff.getChildText("name"))) {
+                rootNode.removeContent(staff);
+            }*/
+
             // if staff id is 1001
             if ("1001".equals(id.trim())) {
-                // remove element role
+                // remove element `role`
                 staff.removeChild("role");
-                // get salary element, update attribute from USD to MYR
+                // update xml element `salary`, update attribute to MYR
                 staff.getChild("salary").setAttribute("currency", "MYR");
             }
 
@@ -78,7 +86,15 @@ public class ModifyXmlJDom {
         // print to console for testing
         XMLOutputter xmlOutput = new XMLOutputter();
         xmlOutput.setFormat(Format.getPrettyFormat());
+
+        // write to console
         xmlOutput.output(doc, System.out);
+
+        // write to a file
+        /*try (FileOutputStream output =
+                     new FileOutputStream("c:\\test\\staff-update.xml")) {
+            xmlOutput.output(doc, output);
+        }*/
 
     }
 
